935ce37017cb5356888b3ff79f4a9e801d2187ca
zxcvkim <190175095+asdfkim@users.noreply.github.com>
2026-06-28 16:03:23 +0900

    🎉 Assemble!!! `(main.rs)`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::time::Instant;
+
+use proof_of_work::{Block, BlockChain};
+
 fn main() {
-    println!("Hello, world!");
+    let chain = BlockChain::new();
+
+    let number_of_blocks = 100;
+    let difficulty = 4;
+
+    for i in 1..=number_of_blocks {
+        let start_time = Instant::now();
+
+        let prev_hash = chain.latest_block().unwrap().hash();
+        let content = format!("Block #{}", i);
+        let mut new_block = Block::new(prev_hash, content.clone(), difficulty, 0);
+
+        if new_block.mine(u64::MAX) {
+            let duration = start_time.elapsed();
+            let message = format!(
+                "{} mined successfully! (Nonce: {}, Time: {:.3}s",
+                content,
+                new_block.nonce(),
+                duration.as_secs_f32()
+            );
+            println!("{}", message);
+        }
+    }
 }
